719f8a6f7358c868e3d56e32348e4bfd651e410d
Thomas Zanis <56691946+campingtomz@users.noreply.github.com>
2020-09-02 11:15:46 -0400

    Initial commit

---
+++ RogueFinancialPortal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RogueFinancialPortal.MvcApplication" Language="C#" %>
---
+++ RogueFinancialPortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RogueFinancialPortal.Startup))]
+namespace RogueFinancialPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
